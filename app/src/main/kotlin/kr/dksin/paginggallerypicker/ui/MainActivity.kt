3a731fd5e95620e608679937669a815cc4acfc60
dkshin <dkshin@neowiz.com>
2021-01-25 21:02:24 +0900

    test commit...

--- app/src/main/kotlin/kr/dksin/paginggallerypicker/ui/MainActivity.kt
+++ app/src/main/kotlin/kr/dksin/paginggallerypicker/ui/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //test....
     }
 
     override fun onSupportNavigateUp(): Boolean {
